97401b2d0d0cc003f54ca69feeacf7e80f49445e
Ellen Guo <elleng18@vt.edu>
2021-12-04 22:45:58 -0500

    Working Query Training

--- query_test.py
+++ query_test.py
@@ -2,6 +2,7 @@
 import cv2
 import numpy as np
 import random
+from matplotlib import pyplot as plt
 
 
 # import all profile images from FaceDetection Folder
@@ -98,6 +99,27 @@ def sift_feature(image1, image2):
     return des1, des2
 
 
+def check(predict, target):
+    error = 0
+    for k in range(len(target)):
+        if target[k] != predict[k]:
+            error += 1
+    error = error / len(target) * 100
+    return error
+
+
+def display_img(img, query, predict, title):
+    fig, axs = plt.subplots(len(query), 2)
+    fig.suptitle(title)
+    axs[0, 0].set_title('Queried Image')
+    axs[0, 1].set_title('Predicted Class')
+    for i in range(len(query)):
+        axs[i, 0].imshow(img[query[i]], cmap='gray')
+        axs[i, 0].axis('off')
+        axs[i, 1].imshow(img[predict[i]*10], cmap='gray')
+        axs[i, 1].axis('off')
+
+
 def main():
     images = read_img('FaceDetection')
     index = list(np.arange(40))
@@ -108,49 +130,85 @@ def main():
     train_index = index[0:split]
     test_index = index[split:]
     random.Random(128).shuffle(train_index) # shuffle again
-    np.random.seed(90)
     # randomly select the images to be queried
     sel_index = np.arange(len(train))
-    # boolean array to remove "database" images from query set
-    not_first_arr = (sel_index % 10) != 0
-    rand_train = np.random.choice(sel_index[not_first_arr], size=10, replace=False)
+    count = 0
+    error_arr = []
+    all_train = []
+    all_predicted = []
+    # determine optimal database image value
+    while count < 8:
+        # boolean array to remove "database" images from query set
+        not_first_arr = (sel_index % 10) > count
+        # randomly select query images excluding images used in the database
+        rand_train = np.random.choice(sel_index[not_first_arr], size=10, replace=False)
+        target = []
+        predict = []
+        train_query = []
+        for j in rand_train:
+            train_id = int(j / 10)
+            img_id = j % 10
+            query_id = train_index[train_id] * 10 + img_id
+            target.append(train_index[train_id])
+            train_query.append(query_id)
+            match_per = []
+            # iterating through the first images of the database only
+            for i in np.arange(40)*10:
+                # testing if given a database of multiple images for this person; can SIFT be used to identify them
+                fold_match = []
+                for k in range(count + 1):
+                    fold = k+i
+                    des1, des2 = sift_feature(images[query_id],images[fold])
+                    match_rate = match_fun(des1,des2)
+                    fold_match.append(match_rate)
+                match_per.append(np.average(fold_match))
+            predict.append(match_per.index(max(match_per)))
+        all_train.append(train_query)
+        all_predicted.append(predict)
+        curr_error = check(predict, target)
+        count += 1
+        print('Error for %s Image in Database: %s' % (count, curr_error))
+        error_arr.append(curr_error)
+
+    # Testing
+    random.Random(64).shuffle(test_index) # shuffle again
+    # randomly select the images to be queried
+    sel_test = np.arange(len(test))
+    # determine optimal database image count
+    val = error_arr.index(min(error_arr)) + 1
+    print('Best Image Number in Database Value: %s' %val)
+    remove = (sel_test % 10) > val
+    # randomly select query images excluding images used in the database
+    rand_test = np.random.choice(sel_test[remove], size=10, replace=False)
     target = []
     predict = []
-    for j in rand_train:
-        train_id = int(j / 10)
+    test_query = []
+    for j in rand_test:
+        test_id = int(j / 10)
         img_id = j % 10
-        query_id = train_index[train_id] * 10 + img_id
-        target.append(train_index[train_id])
+        query_id = train_index[test_id] * 10 + img_id
+        target.append(train_index[test_id])
+        test_query.append(query_id)
         match_per = []
         # iterating through the first images of the database only
         for i in np.arange(40)*10:
-            d1,d2 = sift_feature(images[query_id],images[i])
-            match_rate = match_fun(d1,d2)
-            # perform folder match and skip over the particular image index: avoid 100% match
-            # average the matches and find the highest ... matched folder == predicted individual
             # testing if given a database of multiple images for this person; can SIFT be used to identify them
-            match_per.append(match_rate)
+            fold_match = []
+            for k in range(count + 1):
+                fold = k+i
+                des1, des2 = sift_feature(images[query_id],images[fold])
+                match_rate = match_fun(des1,des2)
+                fold_match.append(match_rate)
+            match_per.append(np.average(fold_match))
         predict.append(match_per.index(max(match_per)))
+    test_error = check(predict, target)
+    print('Test Error: ', test_error)
 
-    error = 0
-    for k in range(len(target)):
-        if target[k] != predict[k]:
-            error += 1
-    error = error / len(target) * 100
-    print("Error Rate: ", error)
-        #     max_match = []
-        #     if (match_rate>55):
-        #         for k in range(10):
-        #             fold = k+i
-        #             des1, des2 = sift_feature(images[query_id],images[fold])
-        #             match_rate = match_fun(des1,des2)
-        #             max_match.append(match_rate)
-        #         maximum = max(max_match)
-        #         ind_max = max_match.index(maximum)
-        #         fold_match.append([ind_max+i,maximum])
-        # for i in range(len(fold_match)):
-        #     if (fold_match[i][1] > 90):
-        #         print(fold_match[i][0])
+    for i in range(len(all_train)):
+        display_img(images, all_train[i], all_predicted[i], '%s Image in Database' %(i+1))
+
+    display_img(images, test_query, predict, 'Test Set Results')
+    plt.show()
 
 
 # Press the green button in the gutter to run the script.
